3f8cdcd9467410657b341e11a67912517bac8ff2
mugmold <mugmold@gmail.com>
2024-11-13 15:14:23 +0700

    update CRUD

--- twtcloneapp/models.py
+++ twtcloneapp/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Data(models.Model) :
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     username = models.CharField(max_length=20)
     content = models.TextField(default="")
